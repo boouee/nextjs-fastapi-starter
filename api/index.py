d041c4462064375dca2faba84d1ba02a562915c3
boouee <104202645+boouee@users.noreply.github.com>
2024-09-29 10:30:12 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -42,7 +42,7 @@ async def check_lead(client, name):
     response = await client.get(url + 'leads?filter[name]=' + name, headers=headers)
     #return json.loads(response)
     if not response:
-      return json.dumps({})
+      return {}
     return response.json()
 
 async def post_lead(client, data):
