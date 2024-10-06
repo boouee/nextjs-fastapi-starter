cd12dd52f7f7f6cdda04813e381d2d0cca68a07e
boouee <104202645+boouee@users.noreply.github.com>
2024-10-06 13:51:15 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -50,7 +50,7 @@ async def get_leads(client):
     #return json.loads(response)   
     if response.status_code == 204:
       return response.status_code
-    return response.content[0].links
+    return str(json.loads(response.text)[0].links)
 
 async def post_lead(client, data):
     data = {
